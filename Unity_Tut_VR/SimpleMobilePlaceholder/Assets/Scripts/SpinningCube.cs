3ddc76582d977cb487566a891e2ebc73f1bcf41c
Jared <rameyjared@gmail.com>
2017-01-09 23:39:24 -0800

    Basic mobile controls set up
    Playing around with basic mobile controls. Have a small cube moving left
    and right, up and down like in a 2D platformer.

---
+++ Unity_Tut_VR/SimpleMobilePlaceholder/Assets/Scripts/SpinningCube.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SpinningCube : MonoBehaviour 
+{
+	public float m_Speed = 20f;
+    public float MoveForce = 0.0f;
+
+    private Rigidbody CubeBody;
+	private Vector3 m_RotationDirection = Vector3.up;
+
+    private void Start()
+    {
+        CubeBody = gameObject.GetComponent<Rigidbody>();
+    }
+
+	public void ToggleRotationDirection()
+	{
+		Debug.Log ("Toggling rotation direction");
+
+		if (m_RotationDirection == Vector3.up) 
+		{
+			m_RotationDirection = Vector3.down;
+		}
+		else 
+		{
+			m_RotationDirection = Vector3.up;
+		}
+	}
+
+    public void Toggle_MoveLeft()
+    {
+        Debug.Log("Moving Left");
+        CubeBody.AddForce((Vector3.left * MoveForce));
+    }
+
+	void Update() 
+	{
+		transform.Rotate(m_RotationDirection * Time.deltaTime * m_Speed);
+	}
+}
---
+++ Unity_Tut_VR/Simple_MobileGame/Assets/Scripts/Controller_Test.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Controller_Test : MonoBehaviour
+{
+    public float MoveForce = 0.0f;
+    public float JumpForce = 0.0f;
+    private bool canJump;
+    [HideInInspector]
+    private float JoystickForce = 0.0f;
+    [HideInInspector]
+    private Rigidbody2D PlayerBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        PlayerBody = gameObject.GetComponent<Rigidbody2D>();
+        canJump = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        JoystickForce = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        if (CrossPlatformInputManager.GetButton("Jump") && canJump)
+        {
+            canJump = !canJump;
+            Debug.Log("Jumping");
+            PlayerBody.AddForce((Vector2.up) * (MoveForce * 100));
+        }
+
+        PlayerBody.AddForce((-Vector2.left * MoveForce) * JoystickForce);   
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        //Debug.Log(coll.gameObject.layer.ToString());
+
+        if (coll.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            Debug.Log("Collision");
+            if(!canJump)
+            {
+                canJump = !canJump;
+            }
+        }
+    }
+}
