b775d47d793bacd231cb249f42643db4ea5d3d71
Jared <rameyjared@gmail.com>
2017-01-10 03:23:13 -0800

    Mobile_Contoller test2
    This mobile controller is a joystick that moves a ball around in an
    area. Might take this and turn it into a small mobile puzzle game where
    the player moves a ball through a maze

---
+++ Unity_Tut_VR/Mobile_RollingBall/Assets/Scripts/Ball_Controller.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Ball_Controller : MonoBehaviour
+{
+    public float moveForce = 0.0f;
+    [HideInInspector]
+    private Rigidbody playerBody;
+    [HideInInspector]
+    private float joyStickForceX = 0.0f, joyStickForceZ = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerBody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        joyStickForceX = CrossPlatformInputManager.GetAxis("Horizontal");
+        joyStickForceZ = CrossPlatformInputManager.GetAxis("Vertical");
+
+        playerBody.AddForce((-Vector3.left * moveForce) * joyStickForceX);
+        playerBody.AddForce((Vector3.forward * moveForce) * joyStickForceZ);
+    }
+}
