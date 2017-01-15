edf7cc1266ec79c388498b38acf9d82e148bb2a9
Jared <rameyjared@gmail.com>
2017-01-14 21:55:30 -0800

    Attempting Build
    Trying to get unity to build .apk to transfer onto phone but getting
    stange error code. Have to look into it more.

---
+++ Unity_Tut_VR/Mobile_RollingBall/Assets/Scripts/Camera_Contoller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_Contoller : MonoBehaviour
+{
+    [HideInInspector]
+    private GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(player.transform.position.x, gameObject.transform.position.y, player.transform.position.z);
+    }
+}
---
+++ Unity_Tut_VR/Mobile_RollingBall/Assets/Scripts/Finish.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+public class Finish : MonoBehaviour
+{
+    [HideInInspector]
+    private Vector3 startPos = new Vector3(0, 0, 0);
+    [HideInInspector]
+    private GameObject player = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        player   = GameObject.Find("Player");
+        if (player == null)
+        {
+            Debug.Log("Player not found!");
+        }
+        else
+        {
+            startPos = player.transform.position;
+            Debug.Log(startPos);
+        }
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider coll)
+    {
+        if(coll.gameObject == player)
+        {
+            Debug.Log("You Win!");
+            player.transform.position = startPos;
+        }
+    }
+}
